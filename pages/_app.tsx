99f5e8408243f2de8d079ed72f6291095166cc00
Adam Sobotka <vorcigernix@gmail.com>
2022-11-12 22:51:39 +0100

    just recommit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,14 +6,17 @@ import useWalletConnectEventsManager from "../hooks/useWalletConnectEventsManage
 import { createLegacySignClient } from "../utils/LegacyWalletConnectUtil";
 
 export default function App({ Component, pageProps }: AppProps) {
-    // Step 1 - Initialize wallets and wallet connect client
-    const initialized = useInitialization()
+  // Step 1 - Initialize wallets and wallet connect client
 
-    // Step 2 - Once initialized, set up wallet connect event manager
-    useWalletConnectEventsManager(initialized)
-  
-    // Backwards compatibility only - create a legacy v1 SignClient instance.
-    createLegacySignClient()
+  const initialized = useInitialization();
+
+  // Step 2 - Once initialized, set up wallet connect event manager
+
+  useWalletConnectEventsManager(initialized);
+
+  // Backwards compatibility only - create a legacy v1 SignClient instance.
+
+  createLegacySignClient();
   return (
     <>
       <Component {...pageProps} />
